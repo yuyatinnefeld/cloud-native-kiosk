cda71ecf6edfaab4c946cf1ba6db91c57e6852b0
yuyatinnefeld <yuyawamo@gmail.com>
2023-01-09 07:31:49 +0100

    feat: update be service with router and settings

--- deploy/backend/app/main.py
+++ deploy/backend/app/main.py
@@ -1,15 +1,33 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+from app.routes.info import routes as info_routes
+from app.routes.item import routes as item_routes
+
+
+app = FastAPI(
+    title = "cloud native kiosk backend",
+    version = "1.0.0",
+    redoc_url = None,
+    openaip_url = None
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8000",
+        "http://backend.c-native-kiosk.com"
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"hello": "world"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(info_routes.router)
+app.include_router(item_routes.router)
