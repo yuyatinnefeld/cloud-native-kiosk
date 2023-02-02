bec8628e5efef4c85036f9028c497420e6d33c36
Yuya <86715496+yuyatinnefeld@users.noreply.github.com>
2023-02-02 07:45:35 +0100

    test: update unit test with own src entpoint - #40
    use the entpoint '/unittests' from app/main.py

--- deploy/frontend/tests/test_frontend.py
+++ deploy/frontend/tests/test_frontend.py
@@ -1,18 +1,19 @@
 import pytest
 from flask import Flask
+from app import main
 
 def test_app_exists():
-    app = Flask(__name__)
+    app = main.app
     assert app is not None
 
 def test_app_route():
-    app = Flask(__name__)
+    app = main.app
 
-    @app.route("/")
-    def index():
-        return "Hello, World!"
+    @app.route("/unittests")
+    def unittests():
+        return "This is for Unittests"
 
     client = app.test_client()
-    response = client.get("/")
+    response = client.get("/unittests")
     assert response.status_code == 200
-    assert response.data == b"Hello, World!"
+    assert response.data == b"This is for Unittests"
