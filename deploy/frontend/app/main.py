e67df2fdd6b7c55eb97075b61808f980fcc2eac0
Yuya Tinnefeld <yuyatinnefeld@gmail.com>
2023-02-08 19:11:38 +0100

    refactor: update all files with pre-commit

--- deploy/frontend/app/main.py
+++ deploy/frontend/app/main.py
@@ -7,12 +7,12 @@ from flask_login import LoginManager, UserMixin
 login_manager = LoginManager()
 
 app = Flask(__name__)
-app.secret_key = 'key'
+app.secret_key = "key"
 
 login_manager.init_app(app)
 
 users = {
-    'user': {'pw': 'pwd'},
+    "user": {"pw": "pwd"},
 }
 
 
@@ -32,46 +32,46 @@ def user_loader(username):
 
 @login_manager.request_loader
 def request_loader(request):
-    username = request.form.get('username')
+    username = request.form.get("username")
     if username not in users:
         return
     else:
         user = User()
         user.id = username
-        user.is_authenticated = request.form['pw'] == users[username]['pw']
+        user.is_authenticated = request.form["pw"] == users[username]["pw"]
         return user
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        if request.form.get('pw') == users[username]['pw']:
+    if request.method == "POST":
+        username = request.form.get("username")
+        if request.form.get("pw") == users[username]["pw"]:
             user = User()
 
         user.id = username
         flask_login.login_user(user)
-        return redirect(url_for('protect'))
+        return redirect(url_for("protect"))
     else:
-        return render_template('index.html')
+        return render_template("index.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     flask_login.logout_user()
-    return 'Logged out'
+    return "Logged out"
 
 
-@app.route('/home')
+@app.route("/home")
 @flask_login.login_required
 def protect():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/account')
+@app.route("/account")
 @flask_login.login_required
 def account():
-    return render_template('account.html')
+    return render_template("account.html")
 
 
 @app.route("/unittests")
