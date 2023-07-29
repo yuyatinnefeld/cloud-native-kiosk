447c8dafdf3bc6e5c34ac2aa529d64d7d099f0df
yuyatinnefeld <yuyawamo@gmail.com>
2023-07-29 11:49:49 +0200

    fix: update mkdocs config for python-app

--- deploy/backend/python-app/docs/gen_mkdocs_pages.py
+++ deploy/backend/python-app/docs/gen_mkdocs_pages.py
@@ -1,13 +1,17 @@
 """Generate the markdown files and nav automatically"""
-from pathlib import Path
+
 import mkdocs_gen_files
+from pathlib import Path
 
-
+# define root path
 src_root = Path("app")
+
+
 for path in src_root.glob("**/*.py"):
-    doc_path = Path("docs", path.relative_to(src_root)).with_suffix(".md")
+    print(path)
+    doc_path = Path("docs", path).with_suffix(".md")
 
     with mkdocs_gen_files.open(doc_path, "w") as f:
         script_path = ".".join(path.with_suffix("").parts)
-        print(f"# {script_path}.py", file=f)
+        print(f" {script_path}.py", file=f)
         print("::: " + script_path, file=f)
